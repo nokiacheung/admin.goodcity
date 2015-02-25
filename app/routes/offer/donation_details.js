7c23c44168d3c05470be2630e1a839e1c04164b6
sunil-sharma <sunil.sharma@kiprosh.com>
2015-02-25 20:23:16 +0530

    GCW-160 comment#Moved common routes to addon

--- app/routes/offer/donation_details.js
+++
@@ -1,9 +0,0 @@
-import AuthorizeRoute from './../authorize';
-
-export default AuthorizeRoute.extend({
-
-  model: function() {
-    var offerId = this.modelFor('offer').get('id');
-    return this.store.getById('offer', offerId);
-  }
-});
