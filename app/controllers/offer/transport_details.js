5d0d709a85dc6ef18b4f825ce68d623150335cac
swatijadhav <swati.jadhav@kiprosh.com>
2015-02-26 18:46:00 +0530

    use transport-details controller from goodcity-addon

--- app/controllers/offer/transport_details.js
+++ app/controllers/offer/transport_details.js
@@ -1,30 +1,3 @@
-import Ember from 'ember';
+import transportDetails from 'ember-goodcity/controllers/offer/transport_details';
 
-var transportDetails =  Ember.ObjectController.extend({
-  delivery: Ember.computed.alias('model.delivery'),
-
-  user: function(){
-    var userId = this.session.get("currentUser.id");
-    return this.store.getById('user_profile', userId);
-  }.property().volatile(),
-
-  userName: function(){
-    return this.get('delivery.contact.name') || this.get("user.fullName");
-  }.property('contact.name', 'user'),
-
-  userMobile: function(){
-    return this.get('delivery.contact.mobile') || this.get("user.mobile");
-  }.property('contact.mobile', 'user'),
-
-  district: function(){
-    return this.get('delivery.contact.address.district.name') || this.get("user.address.district.name");
-  }.property('user', 'delivery'),
-
-  actions: {
-    handleBrokenImage: function() {
-      this.get("reviewedBy").set("hasImage", null);
-    }
-  }
-});
-
-export default transportDetails;
+export default transportDetails.extend();
