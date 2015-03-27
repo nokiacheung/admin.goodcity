9fed0e0a4d6e7e328b5a18ec9d854cc09a3959ad
Michael McDonald <mcm-ham@users.noreply.github.com>
2015-03-27 22:02:24 +0800

    Fix issue where first package wasn't appearing immediately after creation under receive

--- app/components/receive-item.js
+++ app/components/receive-item.js
@@ -1,21 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: "li",
+  classNameBindings: ["hidden"],
   itemId: null,
   state: null,
   store: Ember.inject.service(),
+  hidden: Ember.computed.empty("packages"),
+  hasMultiplePackages: Ember.computed.gte("packages.length", 2),
 
   item: function() {
     return this.get("store").getById("item", this.get("itemId"));
-  }.property(),
+  }.property("itemId"),
 
   packages: function() {
-    // if it's second time function is called it means packages array has changed
-    if (this.get("loaded")) {
-      this.sendAction("refresh");
-    }
-    this.set("loaded", true);
-
     return this.get("item.packages").filterBy("state", this.get("state"));
-  }.property("item.packages.@each.state")
+  }.property("state", "item", "item.packages.@each.state")
 });
--- app/controllers/review_offer/receive.js
+++ app/controllers/review_offer/receive.js
@@ -1,22 +1,7 @@
-import Ember from 'ember';
+import Ember from "ember";
 
 export default Ember.ObjectController.extend({
   queryParams: ["state"],
   state: "expecting",
-
-  items: function(key, value) {
-    return arguments.length > 1 ? value : this.getItems();
-  }.property("state", "model.items.[]"),
-
-  getItems: function() {
-    return this.get("model.items").filter(i => {
-      return i.get("packages").filterBy("state", this.get("state")).get("length") > 0;
-    });
-  },
-
-  actions: {
-    refresh: function() {
-      this.set("items", this.getItems());
-    }
-  }
+  items: Ember.computed.filterBy("model.items", "state", "accepted")
 });
