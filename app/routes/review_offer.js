1ed3a243aa3be388abf6b103d5cd9bf7974746bf
swatijadhav <swati.jadhav@kiprosh.com>
2015-03-31 20:37:48 +0530

    handle 404 error for non-existing offer and items

--- app/routes/review_offer.js
+++ app/routes/review_offer.js
@@ -5,6 +5,6 @@ export default AuthorizeRoute.extend({
   model: function() {
     var offerId = this.modelFor('offer').get('id');
     var offer = this.store.getById('offer', offerId);
-    return (offer ? offer : this.resourceNotFound("offer"));
+    return offer || this.store.find('offer', offerId);
   }
 });
