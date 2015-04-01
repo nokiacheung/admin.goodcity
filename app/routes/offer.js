85899118fbb86359f09a1d8a10ac09b931abae5b
swatijadhav <swati.jadhav@kiprosh.com>
2015-04-01 13:31:51 +0530

    refactored offer route

--- app/routes/offer.js
+++ app/routes/offer.js
@@ -2,7 +2,6 @@ import AuthorizeRoute from './authorize';
 
 export default AuthorizeRoute.extend({
   model: function(params) {
-    var offer = this.store.getById('offer', params.offer_id);
-    return offer || this.store.find('offer', params.offer_id);
+    return this.store.find('offer', params.offer_id);
   }
 });
