59e088c3e8a147c969e3f423ed0c1745f6e84a2b
swatijadhav <swati.jadhav@kiprosh.com>
2015-04-09 17:52:57 +0530

    display all offers details of donor

---
+++ app/controllers/review_offer/donor_details.js
@@ -0,0 +1,9 @@
+import Ember from "ember";
+
+export default Ember.Controller.extend({
+  donor: null,
+
+  offersCount: function() {
+    return this.get("model.length") + 1;
+  }.property('model.length'),
+});
--- app/routes/review_offer/donor_details.js
+++ app/routes/review_offer/donor_details.js
@@ -1,9 +1,26 @@
 import AuthorizeRoute from './../authorize';
+import AjaxPromise from './../../utils/ajax-promise';
 
 export default AuthorizeRoute.extend({
   model: function() {
+    var _this = this;
     var offerId = this.modelFor('reviewOffer').get('id');
-    var offer = this.store.getById('offer', offerId);
-    return offer.get('createdBy');
+    var currentOffer = this.store.getById('offer', offerId);
+    var donor = currentOffer.get('createdBy');
+
+    return new AjaxPromise("/offers/donor_offers", "GET", this.get('session.authToken'), {donor: donor.get('id')})
+      .then(function(data) {
+        _this.store.pushPayload(data);
+
+        return _this.store.filter('offer', function(offer) {
+          return offer.get('createdBy') === donor && offer.get('state') !== "draft" && offer.get("id") !== currentOffer.get("id");
+        });
+      });
+  },
+
+  setupController: function(controller, model){
+    controller.set("model", model);
+    controller.set("donor", model.get('firstObject.createdBy'));
   }
+
 });
