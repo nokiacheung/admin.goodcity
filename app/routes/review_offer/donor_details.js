ea2ec47e6fd79ffcf44e226f41381b967eb45f76
swatijadhav <swati.jadhav@kiprosh.com>
2015-04-10 16:23:25 +0530

    remove ajax and use restful api call for donor-details

--- app/routes/review_offer/donor_details.js
+++ app/routes/review_offer/donor_details.js
@@ -1,21 +1,12 @@
 import AuthorizeRoute from './../authorize';
-import AjaxPromise from './../../utils/ajax-promise';
 
 export default AuthorizeRoute.extend({
   model: function() {
-    var _this = this;
     var offerId = this.modelFor('reviewOffer').get('id');
     var currentOffer = this.store.getById('offer', offerId);
     var donor = currentOffer.get('createdBy');
 
-    return new AjaxPromise("/offers/donor_offers", "GET", this.get('session.authToken'), {donor: donor.get('id')})
-      .then(function(data) {
-        _this.store.pushPayload(data);
-
-        return _this.store.filter('offer', function(offer) {
-          return offer.get('createdBy') === donor && offer.get('state') !== "draft" && offer.get("id") !== currentOffer.get("id");
-        });
-      });
+    return this.store.find('offer', { donated_by_id: donor.get('id'), include_deleted: true });
   },
 
   setupController: function(controller, model){
