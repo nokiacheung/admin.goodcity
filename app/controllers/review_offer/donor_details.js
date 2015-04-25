f8c1c6b5b2364ac6d1352f2adcb61c70c35fe4a4
Michael McDonald <mcm-ham@users.noreply.github.com>
2015-04-25 17:29:42 +0800

    GCW-311 Create phone view and use for displaying phone number fields

--- app/controllers/review_offer/donor_details.js
+++ app/controllers/review_offer/donor_details.js
@@ -5,10 +5,5 @@ export default Ember.Controller.extend({
 
   offersCount: function() {
     return this.get("model.length") + 1;
-  }.property('model.length'),
-
-  donorMobile: function(){
-    var mobile = this.get("donor.mobile").split("+852")[1];
-    return mobile.substr(0, 4) + " " + mobile.substr(4);
-  }.property('donor'),
+  }.property('model.length')
 });
