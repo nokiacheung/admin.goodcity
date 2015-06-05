c11bf619b8521ee4f6204841856e0594ea4b9c6b
swatijadhav <swati.jadhav@kiprosh.com>
2015-06-05 18:03:25 +0530

    transition effect for submitted-offers to review-offer

--- app/transitions.js
+++ app/transitions.js
@@ -4,4 +4,11 @@ export default function(){
     this.toValue(true),
     this.use('toUp', { duration: 500 })
   );
+
+  this.transition(
+    this.fromRoute('offers.submitted'),
+    this.toRoute('review_offer.items'),
+    this.use('toRight', { duration: 3000 }),
+    this.reverse('toLeft')
+  );
 }
