b7fe9796682d8dd44f9b2dc113b7e14b98f4ed65
Michael McDonald <mcm-ham@users.noreply.github.com>
2015-07-07 15:32:49 +0800

    Use computed property for local storage because it handles cases where local storage is not available

--- app/routes/search.js
+++ app/routes/search.js
@@ -1,12 +1,13 @@
 import AuthorizeRoute from './authorize';
 
 export default AuthorizeRoute.extend({
+  lastVisitedRoute: Ember.computed.localStorage(),
 
   beforeModel: function() {
     var previousRoutes = this.router.router.currentHandlerInfos;
     var previousRoute = previousRoutes && previousRoutes.pop();
     if(previousRoute) {
-      localStorage["lastVisitedRoute"] = previousRoute.name;
+      this.set("lastVisitedRoute", previousRoute.name);
     }
   }
 });
