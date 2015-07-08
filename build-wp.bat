50916265a3746e3c48ef3bdf7f37dbf4eb3aa642
Steve Kenworthy <steveyken@gmail.com>
2015-07-08 17:20:20 +0800

    Updated steps for Windows build

--- build-wp.bat
+++ build-wp.bat
@@ -1,6 +1,16 @@
-rem ember cordova:prepare
+cd c:\Workspace\shared.goodcity
+call git pull
+call bower install
+call npm install
+
+cd c:\Workspace\admin.goodcity
+call git pull
+call bower install
+call npm install
+
 cd cordova
-git pull
 call cordova prepare windows
 set staging=true& call ember build --environment=production
-cordova run windows -- --phone --device
+
+rem cordova run windows -- --phone --device
+cordova build windows
