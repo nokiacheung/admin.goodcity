7fe3f50609a173cdf81f78966c36a97d6691c84a
swatijadhav <swati.jadhav@kiprosh.com>
2015-08-17 20:24:24 +0530

    update count of offers in tabs of in-progress and scheduled page

--- app/controllers/scheduled.js
+++ app/controllers/scheduled.js
@@ -2,4 +2,22 @@ import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
   scheduleldOffers: true,
+
+  allDeliveries: function() {
+    return this.store.all("delivery").filter(function(delivery){
+      return delivery.get("offer.isScheduled");
+    });
+  }.property(),
+
+  collectionCount: function() {
+    return this.get('allDeliveries').filterBy('isAlternate', true).length;
+  }.property('allDeliveries.@each.deliveryType'),
+
+  ggvCount: function() {
+    return this.get('allDeliveries').filterBy('isGogovan', true).length;
+  }.property('allDeliveries.@each.deliveryType'),
+
+  dropOffCount: function() {
+    return this.get('allDeliveries').filterBy('isDropOff', true).length;
+  }.property('allDeliveries.@each.deliveryType'),
 });
