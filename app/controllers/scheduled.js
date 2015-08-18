6684370f7c13276c1e367ec8fdba2fcea45617b6
swatijadhav <swati.jadhav@kiprosh.com>
2015-08-18 19:34:46 +0530

    fixed issue with scheduled-offers tab count

--- app/controllers/scheduled.js
+++ app/controllers/scheduled.js
@@ -4,20 +4,31 @@ export default Ember.ArrayController.extend({
   scheduleldOffers: true,
 
   allDeliveries: function() {
-    return this.store.all("delivery").filter(function(delivery){
-      return delivery.get("offer.isScheduled");
-    });
+    return this.store.all("delivery");
   }.property(),
 
+  allOffers: function() {
+    return this.store.all("offer");
+  }.property(),
+
+  allValidOffers: function() {
+    return this.get("allOffers").filter(d => !d.get("isFinished"));
+  }.property('allOffers.[]'),
+
+  scheduledDeliveries: function() {
+    this.get('allValidOffers'); // extra call to observe valid-offers
+    return this.get("allDeliveries").filter(d => d.get("offer.isScheduled"));
+  }.property('allValidOffers.@each.isFinished', 'allDeliveries.[]'),
+
   collectionCount: function() {
-    return this.get('allDeliveries').filterBy('isAlternate', true).length;
-  }.property('allDeliveries.@each.deliveryType'),
+    return this.get('scheduledDeliveries').filterBy('isAlternate').length;
+  }.property('scheduledDeliveries.@each.deliveryType'),
 
   ggvCount: function() {
-    return this.get('allDeliveries').filterBy('isGogovan', true).length;
-  }.property('allDeliveries.@each.deliveryType'),
+    return this.get('scheduledDeliveries').filterBy('isGogovan').length;
+  }.property('scheduledDeliveries.@each.deliveryType'),
 
   dropOffCount: function() {
-    return this.get('allDeliveries').filterBy('isDropOff', true).length;
-  }.property('allDeliveries.@each.deliveryType'),
+    return this.get('scheduledDeliveries').filterBy('isDropOff').length;
+  }.property('scheduledDeliveries.@each.deliveryType'),
 });
