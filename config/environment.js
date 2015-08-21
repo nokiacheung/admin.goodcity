ad5ae859acbcddbcc50d07a3565bf66f692110be
Michael McDonald <mcm-ham@users.noreply.github.com>
2015-08-21 12:34:18 +0800

    Add shared.goodcity commit sha as version to config

--- config/environment.js
+++ config/environment.js
@@ -37,6 +37,7 @@ module.exports = function(environment) {
       PRELOAD_TYPES: ["territory"],
       PRELOAD_AUTHORIZED_TYPES: ["package_type","donor_condition","rejection_reason","permission", "timeslot", "gogovan_transport", "crossroads_transport", "version"],
       SHA: process.env.APP_SHA || "00000000",
+      SHARED_SHA:  process.env.APP_SHARED_SHA || "00000000",
       VERSION: "1.0.0",
 
       AIRBRAKE_HOST: "https://errbit.crossroads.org.hk",
