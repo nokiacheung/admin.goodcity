4d4b69f1da119008573ff2a852567fb5d24afe9f
swatijadhav <swati.jadhav@kiprosh.com>
2015-09-01 11:27:28 +0530

    fixed typo at scheduledOffers

--- app/mixins/scheduled_offers.js
+++ app/mixins/scheduled_offers.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Mixin.create({
-  scheduleldOffers: true,
+  scheduledOffers: true,
 
   allDeliveries: function() {
     return this.store.all("delivery");
