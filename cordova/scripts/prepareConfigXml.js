528bdf9110e267f083f2664551172ba6b7d72b03
Steve Kenworthy <steveyken@gmail.com>
2015-10-09 14:59:15 +0800

    Finished converting prepareConfigXml to nodejs

--- cordova/scripts/prepareConfigXml.js
+++ cordova/scripts/prepareConfigXml.js
@@ -13,17 +13,19 @@ module.exports = function(context) {
     if (rootdir) {
         var app_details_path = path.join(rootdir, "appDetails.json");
         var app_details = JSON.parse(fs.readFileSync(app_details_path, 'utf8'));
-        // app_details[environment].url
-        // app_details[environment].version
         config_xml_path = path.join(rootdir, "config.xml");
         if (fs.existsSync(config_xml_path)) {
-            // var data = fs.readFileSync(config_xml_path, 'utf8');
             var parser = new xml2js.Parser();
-            fs.readFile(config_xml_path, function(err, data) {
-                parser.parseString(data, function (err, result) {
+            fs.readFile(config_xml_path, 'utf8', function(err, xmlStr) {
+                if (err) throw err;
+                parser.parseString(xmlStr, function (err, result) {
+                    if (err) throw err;
                     result.widget.name = app_details[environment].name;
-                    // fs.writeFileSync(config_xml_path, result, 'utf8');
-                    console.log(result)
+                    result.widget.$.id = app_details[environment].url;
+                    result.widget.$.version = app_details[environment].version;
+                    var builder = new xml2js.Builder();
+                    var xml = builder.buildObject(result);
+                    fs.writeFileSync(config_xml_path, xml, 'utf8');
                 });
             });
         } else {
