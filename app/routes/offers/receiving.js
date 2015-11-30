60a6d7f4db70235591f9ec572170f567bceafcd2
swatijadhav <swati.jadhav@kiprosh.com>
2015-11-30 20:27:22 +0530

    GCW-639 comment# fixed listing order of receiving offers

--- app/routes/offers/receiving.js
+++ app/routes/offers/receiving.js
@@ -1,6 +1,11 @@
 import AuthorizeRoute from './../authorize';
 
 export default AuthorizeRoute.extend({
+
+  renderTemplate() {
+    this.render('offers.submitted', {controller: 'offers.receiving'});
+  },
+
   model() {
     return this.store.filter('offer', function(offer) {
       return offer.get('isReceiving');
