14cbeabe579e21c70544bf4e0d0dc6e35e8161c7
Michael McDonald <mcm-ham@users.noreply.github.com>
2016-01-29 16:18:19 +0800

    Add ability to print existing inventory number on receive package screen

--- app/components/inventory-number-input.js
+++ app/components/inventory-number-input.js
@@ -1,10 +1,12 @@
 import Ember from 'ember';
 import config from '../config/environment';
+import AjaxPromise from '../utils/ajax-promise';
 
 export default Ember.Component.extend({
 
   attributeBindings: [ "name", "inputId", "value", 'invalid'],
   isCordovaApp: config.cordova.enabled,
+  alert: Ember.inject.service(),
 
   actions: {
 
@@ -20,6 +22,19 @@ export default Ember.Component.extend({
           this.get("alert").show("Scanning failed: " + error);
         }
      );
+    },
+
+    printBarcode() {
+      var loadingView = this.container.lookup('component:loading').append();
+      new AjaxPromise("/packages/print_barcode", "POST", this.get('session.authToken'), {inventory_number: this.get("value")}, {dataType:"text"})
+        .catch(xhr => {
+          if (xhr.status !== 200) {
+            this.get("alert").show(xhr.responseText);
+          } else {
+            throw xhr;
+          }
+        })
+        .finally(() => loadingView.destroy());
     }
 
   }
