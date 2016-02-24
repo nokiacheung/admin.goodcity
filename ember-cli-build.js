c49c183b473f4b9fa1736ce5f2fa0b77347468d1
Michael McDonald <mcm-ham@users.noreply.github.com>
2016-02-24 17:12:31 +0800

    GCW-864 Replaced jquery-timeago with moment since we already have the library

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -48,7 +48,6 @@ module.exports = function(defaults) {
   app.import('bower_components/jquery-placeholder/jquery.placeholder.js');
   app.import('bower_components/jquery.cookie/jquery.cookie.js');
   app.import('bower_components/modernizr/modernizr.js');
-  app.import('bower_components/jquery-timeago/jquery.timeago.js');
   app.import('bower_components/socket.io-client/socket.io.js');
 
   app.import('bower_components/lightgallery/light-gallery/css/lightGallery.css');
