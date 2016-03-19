8cfc516fe6c771b732fc495a395b53fbcc15ade9
Michael McDonald <mcm-ham@users.noreply.github.com>
2016-03-19 15:43:57 +0800

    Remove loading of messages in route since it's now handled in controller Call super in setupController to allow parent routes implementation to change

--- app/routes/offer/supervisor_messages.js
+++ app/routes/offer/supervisor_messages.js
@@ -1,14 +1,7 @@
-import ReadMessagesRoute from './../read_messages';
+import MessagesBaseRoute from 'shared-goodcity/routes/messages_base';
 
-export default ReadMessagesRoute.extend({
+export default MessagesBaseRoute.extend({
   renderTemplate() {
     this.render('offer/donor_messages', {controller: 'offer.supervisor_messages'});
-  },
-
-  model() {
-    var offerId = this.modelFor('offer').get('id');
-    return this.store.filter('message', function(message) {
-      return message.get('offer.id') === offerId && message.get('item') === null && message.get('isPrivate') === true;
-    });
   }
 });
--- app/routes/review_offer/logistics.js
+++ app/routes/review_offer/logistics.js
@@ -3,8 +3,8 @@ import AuthorizeRoute from './../authorize';
 export default AuthorizeRoute.extend({
 
   setupController(controller, model) {
+    this._super(controller, model);
     var defaultGogovanOption = controller.get("defaultGogovanOption");
-    controller.set("model", model);
     controller.set("selectedGogovanOption", defaultGogovanOption);
   },
 });
