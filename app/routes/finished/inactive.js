0b4fa5a186a4c96eb0083ef342ef68aa01b16a1f
swatijadhav <swati.jadhav@kiprosh.com>
2016-03-22 18:03:27 +0530

    revised offers-data load for finished offers

--- app/routes/finished/inactive.js
+++ app/routes/finished/inactive.js
@@ -4,8 +4,4 @@ export default AuthorizeRoute.extend({
   renderTemplate() {
     this.render('my_list.reviewing', {controller: 'finished.inactive'});
   },
-
-  model() {
-    return this.store.query('offer', { states: ["inactive"] });
-  }
 });
