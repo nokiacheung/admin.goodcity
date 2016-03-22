ad9e4aa210f2a6edbb63ba8feb5fb9694b0312d6
swatijadhav <swati.jadhav@kiprosh.com>
2016-03-22 20:13:42 +0530

    fixed test cases for finished offers due to runloop

--- tests/acceptance/finished-test.js
+++ tests/acceptance/finished-test.js
@@ -1,11 +1,21 @@
 import Ember from 'ember';
 import startApp from '../helpers/start-app';
+import FactoryGuy from 'ember-data-factory-guy';
+import TestHelper from 'ember-data-factory-guy/factory-guy-test-helper';
+import '../helpers/custom-helpers';
 
-var App;
+var App, reviewer, offer1, item1, offer2;
 
 module('Finished Offers', {
   beforeEach: function() {
     App = startApp({}, 2);
+    TestHelper.setup();
+    reviewer = FactoryGuy.make('user', { id: 3 });
+
+    offer1 = FactoryGuy.make("offer", {state:"closed", reviewedBy: reviewer});
+    item1 = FactoryGuy.make("item", { state:"rejected", offer: offer1 });
+
+    offer2 = FactoryGuy.make("offer", {state:"received", reviewedBy: reviewer});
   },
   afterEach: function() {
     Ember.run(App, 'destroy');
@@ -16,16 +26,21 @@ test("redirect to finished offers page", function() {
   visit("/offers/finished");
 
   andThen(function(){
-    equal(currentURL(), "/offers/finished/received");
-    equal(find("ul.list li").length, 1);
-    equal(find("ul.list img").length, 1);
 
-    // under-review status
-    equal($('.time_indicator').text().indexOf('Received by ') > 0, true);
-    var itemStatus = $('li.inbox_page:first span.info div:last').text().replace(/\s{1,}/g,' ');
+    var assertions = function() {
+      equal(currentURL(), "/offers/finished/received");
+      equal(find("ul.list li").length, 1);
+      equal(find("ul.list img").length, 1);
 
-    // items accept-reject status
-    equal(itemStatus, " 0 Received, 0 missing, 0 rejected ");
+      // under-review status
+      equal($('.time_indicator').text().indexOf('Received by ') > 0, true);
+      var itemStatus = $('li.inbox_page:first span.info div:last').text().replace(/\s{1,}/g,' ');
+
+      // items accept-reject status
+      equal(itemStatus, " 0 Received, 0 missing, 0 rejected ");
+    };
+
+    runloopFix(assertions);
   });
 });
 
