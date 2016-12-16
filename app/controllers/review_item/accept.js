2e3dab6bf41d433a1a5451400d478d37fb0653c8
trojanh <rajan.tiwari@kiprosh.com>
2016-12-16 19:18:41 +0530

    Fixed the search bar

--- app/controllers/review_item/accept.js
+++ app/controllers/review_item/accept.js
@@ -30,7 +30,7 @@ export default Ember.Controller.extend({
 
   subPackageTypes: Ember.computed('itemType', function(){
     var itemType = this.get("itemType");
-    return itemType.get("allChildPackagesList").apply(itemType).sort('name');
+    return itemType.get("allChildPackagesList").apply(itemType);
   }),
 
   onItemTypeChange: Ember.observer('itemTypeId', function () {
