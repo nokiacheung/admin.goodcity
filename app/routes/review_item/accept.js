04e19324d701aaced6302c493ec8ad4a20e9c06a
trojanh <rajan.tiwari@kiprosh.com>
2017-08-04 17:33:40 +0530

    do not allow edit page acces even from route

--- app/routes/review_item/accept.js
+++ app/routes/review_item/accept.js
@@ -2,7 +2,24 @@ import AuthorizeRoute from './../authorize';
 
 export default AuthorizeRoute.extend({
 
+  afterModel(model, transition){
+
+    var pkg = model.get("packages.firstObject");
+    var cannotSave = pkg.get("hasAllPackagesDesignated") || pkg.get("hasAllPackagesDispatched");
+
+    if(cannotSave){
+      transition.abort()
+      this.get('messageBox').alert(this.get("i18n").t('must_login'), () => {
+        let my_offer = pkg.get("offerId");
+        this.transitionTo('review_offer.items');
+      });
+      return false;
+    }
+  },
+
+
   setupController(controller, model) {
+
     this._super(controller, model);
     controller.notifyPropertyChange("itemTypeId");
   }
