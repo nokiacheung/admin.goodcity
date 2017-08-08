15eaa23e7c23d4df9351411b34e2aed0af85f51a
trojanh <rajan.tiwari@kiprosh.com>
2017-08-08 17:20:48 +0530

    Allow accept page if item is designated/dispatched

--- app/routes/review_item/accept.js
+++ app/routes/review_item/accept.js
@@ -2,24 +2,7 @@ import AuthorizeRoute from './../authorize';
 
 export default AuthorizeRoute.extend({
 
-  afterModel(model, transition){
-
-    var pkg = model.get("packages.firstObject");
-    var cannotSave = pkg.get("hasAllPackagesDesignated") || pkg.get("hasAllPackagesDispatched");
-
-    if(cannotSave){
-      transition.abort();
-      this.get('messageBox').alert(this.get("i18n").t('designated_dispatched_error'), () => {
-        let my_offer = pkg.get("offerId");
-        this.transitionTo('review_offer.items');
-      });
-      return false;
-    }
-  },
-
-
   setupController(controller, model) {
-
     this._super(controller, model);
     controller.notifyPropertyChange("itemTypeId");
   }
