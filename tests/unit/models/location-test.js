4236411baad78cfc6fdefeba9d0bc5b8a1a5438f
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2017-09-19 10:57:29 +0530

    Location model test cases added

---
+++ tests/unit/models/location-test.js
@@ -0,0 +1,21 @@
+import { test, moduleForModel } from 'ember-qunit';
+
+moduleForModel('location', 'Location Model', {
+});
+
+test('check attributes', function(assert){
+  assert.expect(2);
+  var model = this.subject();
+  var area = Object.keys(model.toJSON()).indexOf('area') > -1;
+  var building = Object.keys(model.toJSON()).indexOf('building') > -1;
+
+  assert.ok(building);
+  assert.ok(area);
+});
+
+test('Checking computed properties', function(assert) {
+  assert.expect(1);
+  var location = this.subject({ area: 24, building: 'Office' });
+  assert.equal(location.get('name'), "Office-24");
+});
+
