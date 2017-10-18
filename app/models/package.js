98ad4295da8a9a24bd9779ba8316397b81ccd99f
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2017-10-18 15:34:43 +0530

    GCW-1548 fix push update for packages location

--- app/models/package.js
+++ app/models/package.js
@@ -97,8 +97,8 @@ export default DS.Model.extend({
   }),
 
   hasAllPackagesDispatched: Ember.computed("ordersPackages.@each.quantity", "ordersPackages.@each.state", "ordersPackages.[]", function() {
-    var ordersPackages = this.store.query("ordersPackage", { search_by_package_id: this.get("id") });
-    this.store.pushPayload(ordersPackages);
+    var offer = this.store.findRecord("offer", this.get("offerId"));
+    this.store.pushPayload(offer);
     var received_quantity = this.get("receivedQuantity");
     var totalDispatchedQty = 0;
     var dispatchedOrdersPackages = this.get("ordersPackages").filterBy("state", "dispatched");
