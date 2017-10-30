d6ba93fffd21e84d7ca3fe89d472fd047803958d
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2017-10-30 12:41:21 +0530

    Source map generation

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,7 +6,7 @@ module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     sourcemaps: {
       enabled: true,
-      extensions: ['js', 'css']
+      extensions: ['js']
     },
     fingerprint: {
       extensions: ['js', 'css', 'png', 'jpg', 'gif', 'map'],
