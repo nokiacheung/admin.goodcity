bd27ab75954502f6863700991a49c099a963b262
viveksriram1 <vivek.sriram@kiprosh.com>
2017-12-05 15:54:07 +0530

    fix cli build

---
+++ tests/factories/designations.js
@@ -0,0 +1,26 @@
+import FactoryGuy from 'ember-data-factory-guy';
+import './item';
+
+FactoryGuy.define('designation', {
+  sequences: {
+    id: function(num) {
+      return num;
+    },
+    code: function(num) {
+      var code = 24400 + num;
+      return "L" + code;
+    }
+  },
+  default: {
+    id:               FactoryGuy.generate('id'),
+    code:             FactoryGuy.generate('code'),
+    detailType:       'StockitLocalOrder',
+    status:           'Active',
+    createdAt:        '12/07/2016',
+    updatedAt:        '12/07/2016',
+    orders_packages:   FactoryGuy.hasMany('orders_package')
+  },
+  designation_with_item: {
+    item: FactoryGuy.hasMany('item')
+  }
+});
