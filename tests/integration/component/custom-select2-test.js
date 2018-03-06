777efd011afdd26eadf2359d46ae2bba51e921b7
viveksriram1 <vivek.sriram@kiprosh.com>
2018-03-06 13:21:20 +0530

    add test case for custom-select2 component

---
+++ tests/integration/component/custom-select2-test.js
@@ -0,0 +1,28 @@
+import { test, moduleForComponent } from 'ember-qunit';
+import startApp from '../../helpers/start-app';
+import FactoryGuy from 'ember-data-factory-guy';
+import TestHelper from 'ember-data-factory-guy/factory-guy-test-helper';
+import Ember from 'ember';
+import hbs from 'htmlbars-inline-precompile';
+
+var App, locations, location;
+
+moduleForComponent('radio-text-input', 'Integration | Component | radio text input', {
+  integration: true,
+  beforeEach: function() {
+    App = startApp({}, 2);
+    TestHelper.setup();
+    location = FactoryGuy.make("location");
+    locations = [location];
+    this.render(hbs`{{custom-select2 content=locations record=location recordId=location.id}}`);
+    // {{custom-select2 content=locations record=location recordId=locationId placeholder=(t "receive_package.add_location")}}
+  },
+  afterEach: function() {
+    Ember.run(function() { TestHelper.teardown(); });
+    Ember.run(App, 'destroy');
+  }
+});
+
+test("check didInsert", function(assert){
+  assert.equal(Boolean(this.get('isAndroidDevice')), false);
+});
