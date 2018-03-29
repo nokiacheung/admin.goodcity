e65469f72495a4cdcd32028463fa24ca2ff3fd18
namrataukirde <namrata.ukirde@kiprosh.com>
2018-03-29 18:28:50 +0530

    assign roles to user screen changes

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -3,7 +3,7 @@ const { getOwner } = Ember;
 
 export default Ember.Controller.extend({
   user: Ember.computed.alias('model'),
-  selectedId: null,
+  selectedRoleIds: [],
 
   permissions: Ember.computed(function(){
     return this.store.peekAll("permission").rejectBy("name", "System").sortBy('name');
@@ -13,19 +13,20 @@ export default Ember.Controller.extend({
     return this.store.peekAll("role");
   }),
 
-  userRoleIds: Ember.computed(function(){
-    // return this.store.peekAll("userRole").filterBy("userId", this.get('user').id).mapBy('roleId');
-    return [1,4,5];
-  }),
-
   actions: {
-    saveUser(){
+    setSelecteIds(id, isSelected) {
+      if(isSelected){
+        this.get('selectedRoleIds').push(id);
+      } else {
+        this.get('selectedRoleIds').pop(id);
+      }
+    },
+
+    saveUser(info){
       var user = this.get("model");
-      var selectedId = this.get("selectedId");
-      if(selectedId) {
-        var loadingView = getOwner(this).lookup('component:loading').append();
-        var permission = selectedId === "-1" ? null : this.store.peekRecord('permission', selectedId);
-        user.set("permission", permission);
+      if(this.get('selectedRoleIds.length')) {
+        var loadingView = getOwner(this).lookup('component:loading').append()
+        user.set('userRoleIds', this.get('selectedRoleIds'));
         user.save()
           .then(() => loadingView.destroy())
           .catch(error => {
@@ -37,3 +38,4 @@ export default Ember.Controller.extend({
     }
   }
 });
+
--- app/routes/user.js
+++ app/routes/user.js
@@ -6,9 +6,12 @@ export default AuthorizeRoute.extend({
     return this.store.peekRecord('user', params.user_id);
   },
 
-  setupController(controller, model) {
-    this._super(controller, model);
-    controller.set("selectedId", model.get("permission.id") || "-1");
+  afterModel(model) {
+    return this.store.query('userRole', { search_by_user_id: model.id });
   }
+
+  // setupController(controller, model) {
+  //   this._super(controller, model);
+  // }
 
 });
